dac35dcfd0a34af8fb37dd5b70563ad056525772
root <minhkt2016@gmail.com>
2023-05-05 03:49:04 +0000

    Initial commit

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -7,5 +7,4 @@ GPUS=$3
 PORT=${PORT:-29500}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
+python $(dirname "$0")/test.py $CONFIG $CHECKPOINT ${@:4}
